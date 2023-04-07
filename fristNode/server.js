c9d4a22cef99aff1861af60006c840d78ba07c14
7985575255 <arjun21217@gmail.com>
2023-04-07 15:26:52 +0530

    This file is the redirect to the 302 responsed

--- fristNode/server.js
+++ fristNode/server.js
@@ -2,25 +2,26 @@ const http=require('http');
 const fs= require('fs');
 
 const server=http.createServer((req,res)=>{
-    let path ='index.html';
-    switch(req.url){
-        case '/contect':
-    path +='view.html'
-         break;
-        case '/about':
-    path +='about.html';
-        break;
-        default: '404.html'
-
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+ res.write('<html>');
+ res.write('<head><title>My frist Page</title><head>')
+ res.write('<body><form action="/message" method="POST"> <input type="text" name="message"> <button type="submit"> Button</button></form> </body>')
+ res.write('</html>');
+  return res.end();
     }
- fs.readFile(path,(err,fileData)=>{
-    if(err){
-        console.log(err)
-    }else{
-        res.write(fileData);
-        res.end();
+    if(url==='/message' && method==='POST'){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode=302;
+        return res.end()
     }
- })
+ res.setHeader('Content-Type','text/html');
+ res.write('<html>');
+ res.write('<head><title>My frist Page</title><head>')
+ res.write('<body> <h1> This is a h1 tage </h1> </body>')
+ res.write('</html>');
+ res.end();
 
 })
 server.listen(3000,'localhost',()=>{
