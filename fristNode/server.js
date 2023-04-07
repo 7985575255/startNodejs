80f621647f158cf3cdd2a9fbc3dc68c725b6b8e6
7985575255 <arjun21217@gmail.com>
2023-04-07 06:18:13 +0530

    this commit for node url module

--- fristNode/server.js
+++ fristNode/server.js
@@ -1,6 +1,28 @@
 const http=require('http');
+const fs= require('fs');
 
 const server=http.createServer((req,res)=>{
-    console.log('Arjun verma')
+    let path ='index.html';
+    switch(req.url){
+        case '/contect':
+    path +='view.html'
+         break;
+        case '/about':
+    path +='about.html';
+        break;
+        default: '404.html'
+
+    }
+ fs.readFile(path,(err,fileData)=>{
+    if(err){
+        console.log(err)
+    }else{
+        res.write(fileData);
+        res.end();
+    }
+ })
+
 })
-server.listen(4000)
+server.listen(3000,'localhost',()=>{
+    console.log('server is listen port no 3000 ')
+})
