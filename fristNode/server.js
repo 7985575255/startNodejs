6a9420df0b283f6f45c8ea4f5253dff7b68b9e19
7985575255 <arjun21217@gmail.com>
2023-04-10 08:18:12 +0530

    This code based on the router

--- fristNode/server.js
+++ fristNode/server.js
@@ -1,23 +1,17 @@
 
 const express=require('express');
+const productRoute=require('./routes/product');
+const shoping=require('./routes/shop');
 const body_parser=require('body-parser');
+
  const app=express();
 app.use(body_parser.urlencoded({extended:false}))
+ 
+app.use(productRoute);
+app.use(shoping);
 
-
-app.use('/add-product',(req, res,next)=>{
-    res.send('<form action ="/product" method="POST"> <input type="text" name="title"><button type="submit">Add Product</button></form>')
-    
-})
-
-app.use('/product',(req, res,next)=>{
-    console.log(req.body)
-   res.redirect('/')
-    
-})
-app.use('/',(req, res,next)=>{
-    res.send('<h1> Express is working Now </h1>')
-    
+app.use((req,res,next)=>{
+res.status(404).send('<h1> The page is not found</h1>')
 })
 
 app.listen(3000);
